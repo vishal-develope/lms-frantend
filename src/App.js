04068ad61f31b4ce87d825f32adf1116eb3e0d85
Vishal Patel <patel221007@gmail.com>
2023-08-19 15:22:54 +0530

    add home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <Home/>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+function Home(){
+    return(
+        <div>
+            <h1>Hii</h1>
+        </div>
+    );
+}
+
+export default Home
